c217493b8260330ed5bdd7a8746a336c9659adcc
Mukesh Thawani <mukesh9039@gmail.com>
2017-10-10 09:01:24 +0530

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-TestStaticFramework_Tests/Pods-TestStaticFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestStaticFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestStaticFramework_Tests
+@end
---
+++ Example/Pods/Target Support Files/TestStaticFramework/TestStaticFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestStaticFramework : NSObject
+@end
+@implementation PodsDummy_TestStaticFramework
+@end
